f7ea702f5acbc95d3b7d555366d80074b478c242
Sander van den Oever <sandervdo@gmail.com>
2016-03-13 14:10:26 +0100

    Draft of opdracht1

---
+++ opdracht1/programming101.js
@@ -0,0 +1,25 @@
+// Opdracht 1
+// -------------------------------------------
+// Maak 2 variabelen met de namen 'naam' en 'leeftijd' en geef ze een waarde
+
+
+
+// Tel het aantal jongens en meisjes in de zaal
+var jongens = 0;
+var meisjes = 0;
+
+// Maak een nieuwe variabele met de naam 'ninjas'
+// Tel hierin het aantal meisjes en jongens bij elkaar op
+// Let op, je mag hier GEEN getal neerzetten! Laat de computer rekenen!
+
+
+// Nu gaan we het iets lastiger maken. We gaan met lijsten (arrays) werken
+// Maak een variabele met de naam 'hobbies' en zet hierin een lijst met je hobbies
+
+
+// Dit is een lijst met allemaal namen
+//             0       1        2        3          4         5      6
+var namen = ['Henk', 'Piet', 'Niels', 'Klaas', 'Annemieke', 'Tim', 'Paul'];
+// Maak een variabele met de naam 'champion' en zet daarin de naam van de champion van deze dojo
+// Weet je niet wie dat is? Dan moet je hem eerst zien te vinden en zijn naam vragen!
+// Gebruik voor deze opdracht de lijst (array) hierboven. Je mag niet zelf een naam typen.
